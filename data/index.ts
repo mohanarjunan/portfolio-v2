ca126274b6fd6c0fc4764cb5100e7e8ade1ad00d
Mohan Arjunan <mohan.arjunan@finsurge.tech>
2025-03-05 09:01:20 +0530

    V2.0 Setup

---
+++ data/index.ts
@@ -0,0 +1,187 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My Tech Stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Tailwind & Radix based UI Library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Dr Raajan LifeCare Foundation",
+    des: "It is a Nagercoil-based organization dedicated to enhancing various aspects of life, including personal well-being, mental health, and life skills development.",
+    img: "/p1.svg",
+    iconLists: ["/wix.svg"],
+    link: "/drraajan.com",
+  },
+  {
+    id: 2,
+    title: "Thooya Arokia Matha Church",
+    des: "Thooya Arokia Matha Church in Sachiapuram, near Sivakasi, Tamil Nadu, is a Catholic parish dedicated to Our Lady of Good Health, serving the local community with regular masses and spiritual services.",
+    img: "/p2.svg",
+    iconLists: ["/wix.svg"],
+    link: "/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Mohan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Mohan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Mohan is the ideal partner.",
+    name: "Dr. R. Lawrance",
+    title: "CEO of Cooldreamers",
+  },
+  {
+    quote:
+      "Working with Mohan has been an absolute pleasure. His expertise in full-stack development and cloud solutions transformed our project into a seamless, high-performing application. He is highly professional, detail-oriented, and always goes the extra mile to ensure everything runs flawlessly.",
+    name: "Dr. P. Raajan",
+    title: "Founder of Dr Raajan LifeCare Foundation",
+  }
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "CoolDreamers",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Graphic Designer - Intern",
+    desc: "Company: GlobalMediaSolutions. Assisted in the design props.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full-Stack Web Developer - Intern",
+    desc: "Company: BaboolSoft. Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Junior DevOps Engineer",
+    desc: "Company: Finsurge. Led the dev of a web app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Junior Backend Developer",
+    desc: "Company: Finsurge. Working with the SpringBoot backend development for various complex applications.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+  {
+    id: 4,
+    img: "/instagram.svg",
+  },
+];
